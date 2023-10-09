ee5cfb2417a99705fb043e694c37aa9d71e773bb
pablovilchez <pablovilchez.r@gmail.com>
2023-10-09 19:32:22 +0200

    ok

--- include/minishell.h
+++ include/minishell.h
@@ -6,18 +6,18 @@
 /*   By: pvilchez <pvilchez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 13:01:05 by gkrusta           #+#    #+#             */
-/*   Updated: 2023/10/09 19:26:50 by pvilchez         ###   ########.fr       */
+/*   Updated: 2023/10/09 19:32:04 by pvilchez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-#include <readline/readline.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <readline/history.h>
+# include <readline/readline.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <readline/history.h>
 
-#include "../assets/libft/libft.a"
+# include "../assets/libft/libft.h"
 
 #endif

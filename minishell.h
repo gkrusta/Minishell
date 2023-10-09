669ff8e6241168fa363f52b0aabe2f2886aebda4
Gabriela <krustagabriela@gmail.com>
2023-10-09 13:02:06 +0200

    libary add

--- minishell.h
+++ minishell.h
@@ -1,27 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.c                                        :+:      :+:    :+:   */
+/*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gkrusta <gkrusta@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/09 12:30:52 by gkrusta           #+#    #+#             */
-/*   Updated: 2023/10/09 12:59:39 by gkrusta          ###   ########.fr       */
+/*   Created: 2023/10/09 13:01:05 by gkrusta           #+#    #+#             */
+/*   Updated: 2023/10/09 13:01:41 by gkrusta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
 
 #include <readline/readline.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void)
-{
-	char *str;
-	char* username;
-
-	username = getenv("USER"); 
-	printf("\n\nUSER is: @%s", username); 
-	str = readline("\n>>> "); 
-	//printf()
-	return (0);
-}
+#endif

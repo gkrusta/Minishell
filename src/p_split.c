39e3a9931866d2275188d0d2bced01d816c3dab9
pablovilchez <pablovilchez.r@gmail.com>
2023-10-10 10:04:30 +0200

    ok

--- src/p_split.c
+++ src/p_split.c
@@ -1,13 +1,33 @@
 #include "minishell.h"
 
-/* char	*add_spaces(char *str)
+int	is_special_char(char c)
 {
-	int	i;
+	if(c == '0')
+		return (1);
+	return (0);
+}
+
+char	*add_spaces(char *str)
+{
+	int		i;
+	char	*res;
+	char	*aux;
 
 	i = 0;
+	res = ft_calloc(sizeof(char), 1);
 	while (str[i])
 	{
-		write(1, &str[i], 1);
+		if (is_special(str[i]))
+		{
+			aux = res;
+			res = ft_strjoin(res, ' ');
+			free(aux);
+		}
+		aux = res;
+		res = ft_strjoin(res, str[i]);
+		free(aux);
 	}
+	free(str);
+	ft_printf("Before: %s\n After: %s", str, res);
+	return (res);
 }
- */

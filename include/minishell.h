34f3f9134b370053c2043378b54dc30fe7f378c3
pablovilchez <pablovilchez.r@gmail.com>
2023-10-11 14:19:29 +0200

    ok

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: pvilchez <pvilchez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 13:01:05 by gkrusta           #+#    #+#             */
-/*   Updated: 2023/10/11 14:18:40 by pvilchez         ###   ########.fr       */
+/*   Updated: 2023/10/11 14:19:22 by pvilchez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,19 +23,6 @@
 #include "minishell_structs.h"
 
 
-// structura que va a tener cada commando despues de lexer
-typedef struct		s_cmd
-{
-	char			*cmd; // solo commando
-	char			*cmd_path; // commando + su path
-	int				infile;
-	int				outfile;
-	struct s_cmd	*next; // apunta al siguiente commando
-}					t_cmd;
 
-typedef struct		s_shell
-{
-	char			*path; // va a guardar el path
-}					t_shell;
 
 #endif

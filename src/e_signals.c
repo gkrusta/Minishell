49c01259edbe0e35742f867293f1ffec1b031468
pablovilchez <pablovilchez.r@gmail.com>
2023-11-06 19:31:58 +0100

    ok

--- src/e_signals.c
+++ src/e_signals.c
@@ -5,7 +5,10 @@ void	signal_handler(int signal)
 	if (signal == SIGINT)
 	{
 		if (shell_state)
+		{
 			ft_printf("\nminishell> ");
+			rl_replace_line("\0", 0);
+		}
 		else
 			ft_printf("\nPresionado Ctrl+C durante una tarea\n");
 	}
@@ -28,10 +31,9 @@ void	signal_handler(int signal)
 void	setup_signal_handling(struct sigaction *sa, char *input)
 {
 	sa->sa_handler = signal_handler;
+	sa->sa_flags = 0;
+	sigemptyset(&sa->sa_mask);
 	sigaction(SIGINT, sa, NULL);
 	sigaction(SIGQUIT, sa, NULL);
-}
-
 	//term.c_lflag &= ~ECHO;
-	//	sa.sa_flags = 0;
-	//	sigemptyset(&sa.sa_mask);
+}
--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: pvilchez <pvilchez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 12:30:52 by gkrusta           #+#    #+#             */
-/*   Updated: 2023/11/06 18:10:28 by pvilchez         ###   ########.fr       */
+/*   Updated: 2023/11/06 19:28:48 by pvilchez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,7 @@ int	main(int argc, char **argv, char **envp)
 	int					mode;
 	struct sigaction	sa;
 
-	//setup_signal_handling(&sa);
+	setup_signal_handling(&sa);
 	input = NULL;
 	if (argc > 1)
 		mini_args(argc, argv, &mode);
